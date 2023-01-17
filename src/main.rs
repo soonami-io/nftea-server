a26cb0afc4f60ed714e17b605e99bae8efe89518
David Mood <davidmood@hotmail.com>
2023-01-17 13:10:14 +0330

    [DM] feat: webserver recieving the requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener = 
@@ -6,9 +8,16 @@ fn main() {
         .unwrap(); // error throw a panic for developement, requires error handling in production
     
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-
-        println!("Connection established!");
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
-    println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // 1024 bytes long buffer
+    stream.read(&mut buffer).unwrap(); // unwrap for panic simplicity incase, switch to error handling for production
+    println!( 
+        "Request {}",
+        String::from_utf8_lossy(&buffer[..])
+    );
 }

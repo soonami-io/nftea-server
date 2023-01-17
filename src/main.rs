f021ce54dbf9655916a1b476d2a0bd5cdb55361b
David Mood <davidmood@hotmail.com>
2023-01-17 13:00:27 +0330

    [DM] setting up the server local address to port 7878. testing connection

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::net::TcpListener;
+
+fn main() {
+    let listener = 
+        TcpListener::bind("127.0.0.1:7878")
+        .unwrap(); // error throw a panic for developement, requires error handling in production
+    
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
+    println!("Hello, world!");
+}

d30fcb4cec3b6b47eb081cf7566ac63a6144d725
David Mood <davidmood@hotmail.com>
2023-01-17 13:20:05 +0330

    [DM] feat: adding global debugger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 
+const DEBUGGER: bool = false;
+
 fn main() {
     let listener = 
         TcpListener::bind("127.0.0.1:7878")
@@ -16,8 +18,21 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; // 1024 bytes long buffer
     stream.read(&mut buffer).unwrap(); // unwrap for panic simplicity incase, switch to error handling for production
-    println!( 
-        "Request {}",
-        String::from_utf8_lossy(&buffer[..])
-    );
+    
+    if DEBUGGER {
+        println!( 
+            "Request {}",
+            String::from_utf8_lossy(&buffer[..])
+        );
+    }
+
+    // SAMPLE RESPONSE:
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    // e.g. HTTP/1.1 200 OK\r\n\r\n
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }

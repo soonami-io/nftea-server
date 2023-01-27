56d97ca93ddcbaf131b1f24a5bf46e83dfd24cbf
David Mood <davidmood@hotmail.com>
2023-01-27 14:51:56 +0330

    [DM] refactor: revised the file & error handling for the server. adding capability to initiate POST API for brewing NFT tea

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
+use std::io::{prelude::*};
 use std::fs;
 // use std::thread;
 // use std::time::Duration;
@@ -25,49 +25,79 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+
     let mut buffer = [0; 1024]; // 1024 bytes long buffer
     stream.read(&mut buffer).unwrap(); // unwrap for panic simplicity incase, switch to error handling for production
+    let (crud_type, path) = extract_path_and_crud(&buffer);
     
     if DEBUGGER {
         println!( 
             "Request {}",
             String::from_utf8_lossy(&buffer[..])
         );
+        println!("Crud Type is: {}\nPath is: {}", crud_type, path);
+        println!("Buffer starts with {}: {}", crud_type, buffer.starts_with(crud_type.as_bytes()));
     }
-    let get = b"GET ";
+
+    // let get = b"GET ";
     let get_home =  b"GET / HTTP/1.1\r\n";
-    let get_style =  b"GET /assets/styles/styles.css?v=1.0 HTTP/1.1\r\n";
-    let get_script =  b"GET /assets/scripts/app.js HTTP/1.1\r\n";
+    let get_request = format!(
+        "{} {} HTTP/1.1\r\n",
+        crud_type,
+        path
+    );
+    let server_file_path = format!("html{}", path);
+    let server_file_path = server_file_path.as_str();
+
+    // let get_style =  b"GET /assets/styles/styles.css?v=1.0 HTTP/1.1\r\n";
+    // let get_script =  b"GET /assets/scripts/app.js HTTP/1.1\r\n";
     let get_favicon_ico =  b"GET /assets/image/favicon.ico HTTP/1.1\r\n";
     let get_favicon_svg =  b"GET /assets/image/favicon.svg HTTP/1.1\r\n"; 
-    let post =  b"POST /brew HTTP/1.1\r\n";
-    let (status_line, filename_path) = 
+    // let post =  b"POST /brew HTTP/1.1\r\n";
+    let (mut status_line, mut filename_path) = 
         if buffer.starts_with(get_home) {
             ("HTTP/1.1 200 OK", "html/index.html")
-        } else if buffer.starts_with(get_style) {
-            ("HTTP/1.1 200 OK", "html/assets/styles/styles.css")
-        } else if buffer.starts_with(get_script) {
-            ("HTTP/1.1 200 OK", "html/assets/scripts/app.js")
-        } else if buffer.starts_with(get_favicon_ico) {
-            ("HTTP/1.1 404 NOT FOUND", "html/404.html")
-            // ("HTTP/1.1 200 OK", "html/assets/image/favicon.ico")
-        } else if buffer.starts_with(get_favicon_svg) {
-            ("HTTP/1.1 404 NOT FOUND", "html/404.html")
-            // ("HTTP/1.1 200 OK", "html/assets/image/favicon.svg")
-        } else if buffer.starts_with(post) {
-            ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+        } else if buffer.starts_with(get_request.as_bytes()) {
+            ("HTTP/1.1 200 OK", server_file_path)
         } else {
             ("HTTP/1.1 404 NOT FOUND", "html/404.html")
         };
+        // else if buffer.starts_with(get_script) {
+        //     ("HTTP/1.1 200 OK", "html/assets/scripts/app.js")
+        // } else if buffer.starts_with(get_favicon_ico) {
+        //     ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+        //     // ("HTTP/1.1 200 OK", "html/assets/image/favicon.ico")
+        // } else if buffer.starts_with(get_favicon_svg) {
+        //     ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+        //     // ("HTTP/1.1 200 OK", "html/assets/image/favicon.svg")
+        // } else if buffer.starts_with(post) {
+        //     ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+        // } 
     
-
-    let contents = if buffer.starts_with(get) {
-            fs::read_to_string(filename_path).unwrap()
-        } else if buffer.starts_with(post) {
-            fs::read_to_string(filename_path).unwrap() // function to resolve the /brew parameters
-        } else {
-            fs::read_to_string(filename_path).unwrap()
-        };
+    
+    match std::fs::metadata(filename_path) {
+        Ok(metadata) => {
+            if metadata.is_file() {
+                // continue
+            } else {
+                (status_line, filename_path) = ("HTTP/1.1 404 NOT FOUND", "html/404.html");
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading file: {:?}", e);
+            (status_line, filename_path) = ("HTTP/1.1 404 NOT FOUND", "html/404.html");
+        }
+    }
+    
+    let contents = fs::read_to_string(filename_path).unwrap();
+    
+    // if buffer.starts_with(get) {
+    //     fs::read_to_string(filename_path).unwrap()
+    // } else if buffer.starts_with(post) {
+    //     fs::read_to_string(filename_path).unwrap() // function to resolve the /brew parameters
+    // } else {
+    //     fs::read_to_string(filename_path).unwrap()
+    // };
     
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
@@ -78,4 +108,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn extract_path_and_crud(buffer: &[u8]) -> (String, String) {
+    let reader = String::from_utf8_lossy(&buffer[..]);
+    let request_line_segments: Vec<&str> = reader.split(" ").collect();
+    let crud_type = request_line_segments[0].to_string();
+    let path = request_line_segments[1].to_string();
+
+    (crud_type, path)
 }

2b252b354ebaca755f8f3685aa4f322b5406b7f0
ysirblack <yasir.kara@hotmail.com>
2023-02-14 14:44:16 +0300

    [YK] added content-type header

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -361,7 +361,11 @@ const app_component = {
       };
       this.backend_response.isLoading= true;
       try {
-        const response = await axios.post('https://3.70.32.6/uri', data);
+        const response = await axios.post('https://3.70.32.6/uri', data,{
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        });
         let _respone = response.data;
 
         window.localStorage.setItem("brewed_tea",JSON.stringify(_respone))

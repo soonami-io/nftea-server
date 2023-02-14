5ab6b3caa68abf758258fb5dd1bfc8fec776444c
David Mood <davidmood@hotmail.com>
2023-02-14 14:18:13 +0330

    added OPTIONS to cors

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> std::io::Result<()> {
         .wrap(DefaultHeaders::new().add(("Access-Control-Allow-Origin", "*")))
         .wrap(Cors::default()
         .allow_any_origin()
-        .allowed_methods(vec!["POST"])
+        .allowed_methods(vec!["POST", "OPTIONS"])
         .allowed_headers(vec![
             http::header::AUTHORIZATION,
             http::header::ACCEPT,
